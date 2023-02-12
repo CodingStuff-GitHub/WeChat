5f086df3cdd4a84390e1f0725037a7a0922cea0a
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-02-12 16:14:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,29 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import http from "http";
+import { Server } from "socket.io";
 
-const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const server = http.createServer(app);
 
-//User Routes
+// Initialize a new instance of socket.io by passing the server (the HTTP server) object
+const io = new Server(server);
+
+// User Routes
 import { router as userRoutes } from "./routes/userRoutes.js";
 app.use("/api/v1", userRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/index.html"));
 });
 
-export default app;
+// Listen on the connection event for incoming sockets and log it to the console
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+export default server;
