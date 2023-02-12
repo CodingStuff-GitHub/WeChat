dca1e67ec4c2b4ceb4ca0b4b9a0e1eab9e7fb14a
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-02-12 15:15:31 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { registerUser } from "../controllers/userController.js";
-
-export const router = express.Router();
-
-//Register a user
-router.route("/register").post(registerUser);
