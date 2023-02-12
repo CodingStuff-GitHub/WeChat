4a3bf190d7a6dd2c7f5a82ff443b8443b2cffe7c
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-02-13 01:39:12 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -11,9 +11,24 @@ form.addEventListener("submit", function (e) {
     input.value = "";
   }
 });
+
 socket.on("chat message", function (msg) {
   let item = document.createElement("li");
   item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on("user connected", function () {
+  let item = document.createElement("span");
+  item.textContent = "Someone Connected";
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on("user disconnected", function () {
+  let item = document.createElement("span");
+  item.textContent = "Someone Disconnected";
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
 });
