c78fcae97cc7181307a47e249924118c2a9de4a9
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-03-04 17:22:08 +0530

    Update rootSlice.js

--- frontend/src/other/rootSlice.js
+++ frontend/src/other/rootSlice.js
@@ -1,22 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  count: 0,
+  selectedChatId: 0,
 };
 
 const rootSlice = createSlice({
   name: "rootSlice",
   initialState,
   reducers: {
-    increment: (state, action) => {
-      state.count += action.payload.number;
-    },
-    decrement: (state) => {
-      state.count -= 1;
+    selectChat: (state, action) => {
+      state.selectedChatId = action.payload.selectedChatId;
     },
   },
 });
 
-export const { increment, decrement } = rootSlice.actions;
+export const { selectChat } = rootSlice.actions;
 
 export const rootReducer = rootSlice.reducer;
