6cebb22e6ed720f9549f08fadcb573569d2f89a8
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-03-07 22:48:34 +0530

    Added a ThemeProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
 
 const App = () => {
   return (
@@ -10,7 +28,9 @@ const App = () => {
           path="/"
           element={
             <>
-              <Homepage />
+              <ThemeProvider theme={theme}>
+                <Homepage />
+              </ThemeProvider>
             </>
           }
         />
